c606e492af865701b4015fb487df332c733b0bd5
rEactorMB <146958777+rEactorMB@users.noreply.github.com>
2024-11-22 18:47:10 +0000

    Update LogLikelihoodGauss.m
    Description added

--- LogLikelihoodGauss.m
+++ LogLikelihoodGauss.m
@@ -1,3 +1,4 @@
 function log_sum = LogLikelihoodGauss(trait_values, mean, sigma)
+% Log likelihood value calculation given a Gaussian probability distribution.
 log_sum = sum(log((1 / (sigma * sqrt(2 * pi))) * exp(-((trait_values - mean) .* (trait_values - mean)) / (2 * sigma * sigma))));
 end
